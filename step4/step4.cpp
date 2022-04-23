d88907495f9e6726cb3b1826e4bd51debb376743
lmascelli <lmascelli@gmail.com>
2022-04-23 11:39:52 +0200

    step4 init

---
+++ step4/step4.cpp
@@ -0,0 +1,58 @@
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_values.h>
+
+#include <deal.II/base/function.h>
+#include <deal.II/base/quadrature.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/vector.h>
+
+#include <fstream>
+#include <iostream>
+
+using namespace dealii;
+
+#define USE(X) (void)(X);
+
+template <int dim> class Step4 {
+public:
+  Step4();
+  void run();
+
+private:
+  void make_grid();
+  void setup_system();
+  void assemble_system();
+  void solve();
+  void output_results() const;
+
+  Triangulation<dim> triangulation;
+  FE_Q<dim> fe;
+  DoFHandler<dim> dof_handler;
+
+  SparsityPattern sparsity_pattern;
+  SparseMatrix<double> system_matrix;
+
+  Vector<double> solution;
+  Vector<double> system_rhs;
+};
+
+int main(int argc, char **argv) {
+  USE(argc);
+  USE(argv);
+  return 0;
+}
