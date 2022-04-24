318f625b8377e1dd92cadad6724c14a4f2de17b5
lmascelli <lmascelli@gmail.com>
2022-04-24 10:25:13 +0000

    step4 assemble_system

--- step4/step4.cpp
+++ step4/step4.cpp
@@ -88,11 +88,67 @@ double BoundaryValues<dim>::value(const Point<dim> &p,
   return p.square();
 }
 
-template <int dim> Step4<dim>::Step4() : fe(2), dof_handler(triangulation) {}
+template <int dim> Step4<dim>::Step4() : fe(1), dof_handler(triangulation) {}
 
 template <int dim> void Step4<dim>::make_grid() {
   GridGenerator::hyper_cube<dim>(triangulation, -1, 1);
   triangulation.refine_global(5);
 }
 
-template <int dim> void Step4<dim>::setup_system() {}
+template <int dim> void Step4<dim>::setup_system() {
+  dof_handler.distribute_dofs(fe);
+  DynamicSparsityPattern dynamic_sparsity_pattern(dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern(dof_handler, dynamic_sparsity_pattern);
+  sparsity_pattern.copy_from(dynamic_sparsity_pattern);
+
+  system_matrix.reinit(sparsity_pattern);
+
+  solution.reinit(dof_handler.n_dofs());
+  system_rhs.reinit(dof_handler.n_dofs());
+}
+
+template <int dim> void Step4<dim>::assemble_system() {
+  QGauss<dim> quadrature_formula(fe.degree + 1);
+  RightHandSide<dim> right_hand_side;
+
+  FEValues<dim> fe_values(fe, quadrature_formula,
+                          update_values | dealii::update_gradients |
+                              dealii::update_quadrature_points |
+                              dealii::update_JxW_values);
+  const unsigned int dofs_per_cell = fe.n_dofs_per_cell();
+  FullMatrix<double> cell_matrix(dofs_per_cell, dofs_per_cell);
+  Vector<double> cell_rhs(dofs_per_cell);
+
+  std::vector<types::global_dof_index> local_dof_indices(dofs_per_cell);
+
+  for (const auto &cell : dof_handler.active_cell_iterators()) {
+    fe_values.reinit(cell);
+    cell_matrix = 0;
+
+    for (const unsigned int q_index : fe_values.quadrature_point_indices())
+      for (const unsigned int i : fe_values.dof_indices()) {
+        for (const unsigned int j : fe_values.dof_indices())
+          cell_matrix(i, j) +=
+              (fe_values.shape_grad(i, q_index) *
+               fe_values.shape_grad(j, q_index) * fe_values.JxW(q_index));
+        const auto &x_q = fe_values.quadrature_point(q_index);
+        cell_rhs(i) += (fe_values.shape_value(i, q_index) *
+                        right_hand_side.value(x_q) * fe_values.JxW(q_index));
+      }
+
+    cell->get_dof_indices(local_dof_indices);
+    for (const unsigned int i : fe_values.dof_indices()) {
+      for (const unsigned int j : fe_values.dof_indices()) {
+        system_matrix.add(local_dof_indices[i], local_dof_indices[j],
+                          cell_matrix(i, j));
+      }
+      system_rhs(local_dof_indices[i]) += cell_rhs(i);
+    }
+  }
+
+  std::map<types::global_dof_index, double> boundary_values;
+  VectorTools::interpolate_boundary_values(
+      dof_handler, 0, BoundaryValues<dim>(), boundary_values);
+  MatrixTools::apply_boundary_values(boundary_values, system_matrix, solution,
+                                     system_rhs);
+}
