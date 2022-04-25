a7dc67aa35f2c9def8303ebae7e54692ab31cfa1
lmascelli <lmascelli@gmail.com>
2022-04-25 21:02:18 +0000

    step6 setup_system

--- step6/step6.cpp
+++ step6/step6.cpp
@@ -1,7 +1,81 @@
 #define USE(X) (void)(X);
 
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/fe/fe_values.h>
+
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+#include <deal.II/grid/grid_refinement.h>
+#include <deal.II/grid/tria.h>
+
+#include <deal.II/lac/affine_constraints.h>
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/solver.h>
+#include <deal.II/lac/sparse_matrix.h>
+#include <deal.II/lac/vector.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/error_estimator.h>
+#include <deal.II/numerics/vector_tools.h>
+
+#include <fstream>
+
+using namespace dealii;
+
+template <int dim> class Step6 {
+public:
+  Step6();
+  void run();
+
+private:
+  void setup_system();
+  void assemble_system();
+  void solve();
+  void refine_grid();
+  void output_results(const unsigned int cycle) const;
+
+  Triangulation<dim> triangulation;
+  FE_Q<dim> fe;
+  DoFHandler<dim> dof_handler;
+
+  AffineConstraints<double> constraints;
+  SparseMatrix<double> system_matrix;
+  SparsityPattern sparsity_pattern;
+
+  Vector<double> solution;
+  Vector<double> system_rhs;
+};
+
 int main(int argc, char **argv) {
   USE(argc)
   USE(argv)
   return 0;
+}
+
+template <int dim> Step6<dim>::Step6() : fe(2), dof_handler(triangulation) {}
+
+template <int dim> void Step6<dim>::setup_system() {
+  dof_handler.distribute_dofs(fe);
+
+  solution.reinit(dof_handler.n_dofs());
+  system_rhs.reinit(dof_handler.n_dofs());
+
+  constraints.clear();
+  DoFTools::make_hanging_node_constraints(dof_handler, constraints);
+  VectorTools::interpolate_boundary_values(
+      dof_handler, 0, Functions::ZeroFunction<dim>(), constraints);
+  constraints.close();
+
+  DynamicSparsityPattern dsp(dof_handler.n_dofs());
+  DoFTools::make_sparsity_pattern(dof_handler, dsp, constraints, false);
+
+  sparsity_pattern.copy_from(dsp);
+  system_matrix.reinit(sparsity_pattern);
 }
