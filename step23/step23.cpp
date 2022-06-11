ee44d09e7bf17465afdf96b2f694a1aed7f50390
lmascelli <lmascelli@gmail.com>
2022-06-11 16:22:03 +0200

    step23 init

---
+++ step23/step23.cpp
@@ -0,0 +1,18 @@
+/*
+ * THE SYSTEM
+ *
+ * du/dt - v = 0
+ * dv/dt - lamda(u) = f
+ * u(x, t) = g on dSigma
+ * u(x, 0) = u0(x) on Sigma
+ * v(x, 0) = u1(x) on Sigma
+ *
+ * TIME DISCRETIZATION
+ *
+ * k = t;n - t;n-1
+ *
+ * (u;n - u;n-1)/k - [sigma * v;n + (1-sigma) * v;n-1] = 0
+ * (v;n - v;n-1)/k - [sigma * lambda(u);n + (1-sigma) * lambda(u);n-1] = sigma * f;n + (1 - sigma) * f;n-1
+ *
+ * with sigma = 0.5 --> Crank-Nicolson method
+ */
