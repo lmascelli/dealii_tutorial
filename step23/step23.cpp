23298355912c66928adba3e7b207c4b05f6dbb30
lmascelli <lmascelli@gmail.com>
2022-06-12 20:55:19 +0200

    .

--- step23/step23.cpp
+++ step23/step23.cpp
@@ -12,7 +12,19 @@
  * k = t;n - t;n-1
  *
  * (u;n - u;n-1)/k - [sigma * v;n + (1-sigma) * v;n-1] = 0
- * (v;n - v;n-1)/k - [sigma * lambda(u);n + (1-sigma) * lambda(u);n-1] = sigma * f;n + (1 - sigma) * f;n-1
+ * (v;n - v;n-1)/k - [sigma * lambda(u);n + (1-sigma) * lambda(u);n-1] = sigma *
+ *                                                    f;n + (1 - sigma) * f;n-1
  *
  * with sigma = 0.5 --> Crank-Nicolson method
+ *
+ * v;n = v;n-1 + k(sigma * (f;n + lambda(u);n) + (1-sigma) * (lambda(u);n-1 +
+ *                                                            f;n-1))
+ *
+ * (u;n - u;n-1)/k = 
+ *      sigma * v;n-1 + 
+ *      sigma * k * (sigma* (f;n + lambda(u);n)) +
+ *      sigma * k * (1 - sigma) * lambda(u); n-1 +
+ *      (1-sigma) * v;n-1
+ *
+ * 
  */
