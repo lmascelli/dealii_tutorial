9c62607441500f1f47b450ea9491e9d0f0e5eb78
lmascelli <lmascelli@gmail.com>
2022-06-15 13:23:48 +0200

    .

--- step23/step23.cpp
+++ step23/step23.cpp
@@ -183,8 +183,104 @@ void WaveEquation<dim>::setup_system() {
   matrix_u.reinit(sparsity_pattern);
   matrix_v.reinit(sparsity_pattern);
 
-  MatrixCreator::create_mass_matrix(dof_handler, QGauss<dim>(fe.degree + 1), mass_matrix);
-  MatrixCreator::create_laplace_matrix(dof_handler, QGauss<dim>(fe.degree + 1), laplace_matrix);
+  MatrixCreator::create_mass_matrix(dof_handler, QGauss<dim>(fe.degree + 1),
+                                    mass_matrix);
+  MatrixCreator::create_laplace_matrix(dof_handler, QGauss<dim>(fe.degree + 1),
+                                       laplace_matrix);
 
+  solution_u.reinit(dof_handler.n_dofs());
+  solution_v.reinit(dof_handler.n_dofs());
+  old_solution_u.reinit(dof_handler.n_dofs());
+  old_solution_v.reinit(dof_handler.n_dofs());
+  system_rhs.reinit(dof_handler.n_dofs());
+
+  constraints.close();
+}
+
+template <int dim>
+void WaveEquation<dim>::solve_u() {
+  SolverControl solver_control(1000, 1e-8 * system_rhs.l2_norm());
+  SolverCG<Vector<double>> cg(solver_control);
+
+  cg.solve(matrix_u, solution_u, system_rhs, PreconditionIdentity());
+
+  std::cout << "  u-equation: " << solver_control.last_step()
+            << "  CG iterations." << std::endl;
+}
+
+template <int dim>
+void WaveEquation<dim>::solve_v() {
+  SolverControl solver_control(1000, 1e-8 * system_rhs.l2_norm());
+  SolverCG<Vector<double>> cg(solver_control);
+
+  cg.solve(matrix_v, solution_v, system_rhs, PreconditionIdentity());
+
+  std::cout << "  v-equation: " << solver_control.last_step()
+            << "  CG iterations." << std::endl;
+}
+
+template <int dim>
+void WaveEquation<dim>::output_results() const {
+  DataOut<dim> data_out;
+
+  data_out.attach_dof_handler(dof_handler);
+  data_out.add_data_vector(solution_u, "U");
+  data_out.add_data_vector(solution_v, "V");
+
+  data_out.build_patches();
+
+  const std::string filename =
+      "solution-" + Utilities::int_to_string(timestep_number, 3) + ".vtu";
+
+  DataOutBase::VtkFlags vtk_flags;
+  vtk_flags.compression_level =
+      DataOutBase::VtkFlags::ZlibCompressionLevel::best_speed;
+  data_out.set_flags(vtk_flags);
+  std::ofstream output(filename);
+  data_out.write_vtu(output);
+}
+
+template <int dim>
+void WaveEquation<dim>::run() {
+  setup_system();
+
+  VectorTools::project(dof_handler, constraints, QGauss<dim>(fe.degree + 1),
+                       InitialValuesU<dim>(), old_solution_u);
+  VectorTools::project(dof_handler, constraints, QGauss<dim>(fe.degree + 1),
+                       InitialValuesV<dim>(), old_solution_v);
 }
 }  // namespace Step23
+
+int main(int argc, char **argv) {
+  try {
+    using namespace Step23;
+
+    WaveEquation<2> wave_equation_solver;
+    wave_equation_solver.run();
+  } catch (std::exception &exc) {
+    std::cerr << std::endl
+              << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    std::cerr << "Exception on processing: " << std::endl
+              << exc.what() << std::endl
+              << "Aborting!" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+
+    return 1;
+
+  } catch (...) {
+    std::cerr << std::endl
+              << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    std::cerr << "Unknown exception!" << std::endl
+              << "Aborting!" << std::endl
+              << "----------------------------------------------------"
+              << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
